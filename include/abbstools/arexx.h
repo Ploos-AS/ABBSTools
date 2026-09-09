adc888fa876781e034bf31cb6134a8005f5903e8
pos <pgousdal@users.noreply.github.com>
2026-09-09 20:01:31 +0200

    M1.2: model ARexx result strings

--- include/abbstools/arexx.h
+++ include/abbstools/arexx.h
@@ -5,10 +5,14 @@
 
 #define ABBSTOOLS_AREXX_NO_PORT 10
 #define ABBSTOOLS_AREXX_SETUP_ERROR 20
+#define ABBSTOOLS_AREXX_RESULT_LEN 256
 
 struct AbtRexxResult {
     LONG primary;
     LONG secondary;
+    UBYTE has_text;
+    UBYTE truncated;
+    char text[ABBSTOOLS_AREXX_RESULT_LEN];
 };
 
 int abt_arexx_send(const char *port_name,
